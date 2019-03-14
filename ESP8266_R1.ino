9b77985600d05c3f71dd261cb110f142c37ba402
andy_hung <andy_hung@compal.com>
2019-03-14 16:16:56 +0800

    1. Socket Server Finish 2. RGB LED Control

--- ESP8266_R1.ino
+++ ESP8266_R1.ino
@@ -11,6 +11,10 @@
 
 #define UsePassword_Flag  false
 
+# define LED_R D7
+# define LED_G D6
+# define LED_B D5
+
 const char* ssid = Project_SSID;
 const char* password = Project_PASSWORD;
 const char* dns_host = Project_HOST;
@@ -25,15 +29,48 @@ ESP8266WebServer webServer(TCP_PORT);
 DNSServer dnsServer;
 WebSocketsServer webSocket = WebSocketsServer(Sockets_PORT);
 
-String page = "";
-const byte LEDPin = 2;
+String page = "\
+  <html>\
+    <head>\
+      <script>\
+        var connection = new WebSocket('ws://'+location.hostname+':81/', ['arduino']);\
+        connection.onopen = function() {\
+          connection.send('Connect ' + new Date());\
+        };\
+        connection.onerror = function(error) {\
+          console.log('WebSocket Error ', error);\
+        };\
+        connection.onmessage = function(e) {\
+          console.log('Server: ', e.data);\
+        };\
+        function sendRGB() {\
+          var r = parseInt(document.getElementById('r').value).toString(16);\
+          var g = parseInt(document.getElementById('g').value).toString(16);\
+          var b = parseInt(document.getElementById('b').value).toString(16);\
+          if(r.length < 2) { r = '0' + r; }\
+          if(g.length < 2) { g = '0' + g; }\
+          if(b.length < 2) { b = '0' + b; }\
+          var rgb = '#'+r+g+b;\
+          console.log('RGB: ' + rgb);\
+          connection.send(rgb);\
+        }\
+      </script>\
+    </head>\
+    <body>\
+      LED Control:<br/><br/>\
+      R: <input id=\"r\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>\
+      G: <input id=\"g\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>\
+      B: <input id=\"b\" type=\"range\" min=\"0\" max=\"255\" step=\"1\" oninput=\"sendRGB();\" /><br/>\
+    </body>\
+  </html>\
+";
 
-void setup() {
-  page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
+void setup() {      
   ledConfig();
   serialConfig();
   apConfig();
   dnsConfigBegin();
+  socketConfigBegin();
   apHandle();
   apBegin();
 }
@@ -42,24 +79,27 @@ void loop() {
   dnsServer.processNextRequest();
   webSocket.loop();
   webServer.handleClient();
-  if (Serial.available() > 0){
+  if (Serial.available() > 0) {
     char c[] = {(char)Serial.read()};
     webSocket.broadcastTXT(c, sizeof(c));
   }
 }
 
-void ledConfig(){
- pinMode(LEDPin, OUTPUT);
- digitalWrite(LEDPin, LOW);
+void ledConfig() {
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
 
- delay(500);
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, LOW);
+  digitalWrite(LED_B, LOW);
 }
 
-void serialConfig(){
+void serialConfig() {
   Serial.begin(Serial_Speed);
 }
 
-void apConfig(){
+void apConfig() {
   Serial.println();
 
   Serial.print("ESP8266 mode WIFI_AP ... ");
@@ -69,7 +109,7 @@ void apConfig(){
   Serial.println(WiFi.softAPConfig(apIP, apIP, apNetmask) ? "Ready" : "Failed!");
 
   Serial.print("Setting soft-AP Server configuration ... ");
-  if(UsePassword_Flag){
+  if(UsePassword_Flag) {
     Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
   } else {
     Serial.println(WiFi.softAP(ssid) ? "Ready" : "Failed!");
@@ -79,27 +119,25 @@ void apConfig(){
   Serial.println(WiFi.softAPIP());
 }
 
-void apHandle(){
-  webServer.on("/",handle_Root);
-  webServer.on("/LEDOn",handle_LED_On);
-  webServer.on("/LEDOff",handle_LED_Off);
+void apHandle() {
+  webServer.on("/", handle_Root);
   webServer.onNotFound(handle_NotFound);
 }
 
-void apBegin(){
+void apBegin() {
   webServer.begin();
   Serial.println("HTTP server started");
 }
 
-void dnsConfigBegin(){
+void dnsConfigBegin() {
   Serial.println();
   
-  Serial.print("Setting mDNS Server configuration ... ");
+  Serial.print("Setting DNS Server configuration ... ");
   dnsServer.setTTL(300);
   dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);  
   Serial.println("Ready");
     
-  Serial.print("Setting mDNS Server configuration ... ");
+  Serial.print("Setting DNS Server configuration ... ");
   while (!dnsServer.start(DNS_PORT, dns_host, apIP)) {
     Serial.println("Failed!");
     Serial.print("Wait 1 second try again ...");
@@ -109,63 +147,42 @@ void dnsConfigBegin(){
   Serial.println("DNS server started");
 }
 
-void socketConfigBegin(){
+void socketConfigBegin() {
  webSocket.begin();
  webSocket.onEvent(webSocketEvent);
  Serial.println("Socket server started");
 }
 
-void handle_Root(){
+void handle_Root() {
   webServer.send(200, "text/html", page);
 }
 
-void handle_LED_On(){
-  webServer.send(200, "text/html", page);
-    digitalWrite(LEDPin, HIGH);
-    delay(1000);
-}
-
-void handle_LED_Off(){
-  webServer.send(200, "text/html", page);
-    digitalWrite(LEDPin, LOW);
-    delay(1000);
-}
-
-void handle_NotFound(){
+void handle_NotFound() {
   webServer.send(404, "text/plain", "Not found");
 }
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
-  if (type == WStype_TEXT){
-   for(int i = 0; i < length; i++) Serial.print((char) payload[i]);
-   Serial.println();
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+
+    case WStype_CONNECTED:{
+      IPAddress ip = webSocket.remoteIP(num);
+      Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);      
+      webSocket.sendTXT(num, "Connected");}  // send message to client
+      break;
+
+    case WStype_TEXT:
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+      if(payload[0] == '#') {
+        // we get RGB data
+        // decode rgb data
+        uint32_t rgb = (uint32_t) strtol((const char *) &payload[1], NULL, 16);
+        analogWrite(LED_R, map(((rgb >> 16) & 0xFF), 0, 255, 0, 1023));
+        analogWrite(LED_G, map(((rgb >> 8) & 0xFF), 0, 255, 0, 1023));
+        analogWrite(LED_B, map(((rgb >> 0) & 0xFF), 0, 255, 0, 1023));
+      }
+      break;
   }
 }
-/*
-
-snprintf ( html, 1000,
-
-"<html>\
-  <head>\
-    <meta http-equiv='refresh' content='10'/>\
-    <title>ESP8266 WiFi Network</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; font-size: 1.5em; Color: #000000; }\
-      h1 { Color: #AA0000; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>ESP8266 Wi-Fi Access Point and Web Server Demo</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <p>Brightness: %d%</p>\
-    <p>%s<p>\
-    <p>This page refreshes every 10 seconds. Click <a href=\"javascript:window.location.reload();\">here</a> to refresh the page now.</p>\
-  </body>\
-</html>",
-
-    hr, min % 60, sec % 60,
-    brightness,
-    ledText
-  );
-
-  */
